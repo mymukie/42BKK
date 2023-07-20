6a9ea9fa8a1a2531233185e1fc9dae467262347d
mymukie <tay_tachapon@hotmail.co.th>
2023-07-20 14:53:25 +0700

    first try

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+
+}
+
+void ft_print(int nb)
+{
+    int first;
+    int second;
+
+    if (nb > 9)
+    {
+        first = nb / 10;
+        second = nb / 10;
+        ft_putchar(first + 48);
+        ft_putchar(second + 48);
+    }
+    else
+    {
+        ft_putchar('0');
+        ft_putchar(nb + 46);
+    }
+
+}
+
+void ft_print_comb2()
+{
+    int i;
+    int j;
+    
+    i = 0;
+
+    while(i != 99)
+    {
+        j = i + 1;
+        while(j != 99)
+        {
+            ft_print(i);
+            ft_putchar(' ');
+            ft_print(j);
+            if(i < 98 || j <99)
+            {
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+            j++;
+
+        }
+        i++;
+    }
+
+}
+
+int main()
+{
+    ft_print_comb2();
+    return (0);
+}

2596d0cf8f83717dabb59afb6fb4ac9b15e5e910
mymukie <tay_tachapon@hotmail.co.th>
2023-07-25 11:55:00 +0700

    ft_div_mod.c

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,37 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmulmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/24 22:40:09 by mymukie           #+#    #+#             */
-/*   Updated: 2023/07/25 10:54:47 by tmulmane         ###   ########.fr       */
+/*   Created: 2023/07/25 01:56:09 by tmulmane          #+#    #+#             */
+/*   Updated: 2023/07/25 10:42:07 by tmulmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void    ft_div_mod(int a, int b, int *div, int *mod)
+void ft_rev_int_tab(int *tab, int size)
 {
-    *div = a / b;
-    *mod = a % b;
+    
 }
-/*
-#include <stdio.h>
-
-int main(void)
-{
-    int a;
-    int b;
-    int i;
-    int j;
-
-    a = 100;
-    b = 50;
-    ft_div_mod(a, b, &i, &j);
-    printf("value i= %d\n", i);
-    printf("value i= %d\n", j);
-    return (0);
-}
-*/

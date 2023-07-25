5cff7739ade2f5409749700ba8b49085607364bb
mymukie <tay_tachapon@hotmail.co.th>
2023-07-25 10:55:42 +0700

    ft_ultimate_div_mod.c

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: tmulmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 22:45:24 by mymkuie           #+#    #+#             */
-/*   Updated: 2023/07/24 22:49:23 by tmulmane         ###   ########.fr       */
+/*   Updated: 2023/07/25 10:55:34 by tmulmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,20 @@ void ft_ultimate_div_mod(int *a, int *b)
     tmp = *a / *b;
     *b = *a % *b;
     *a = tmp;
+    //write(1, &tmp, 1);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 100;
+    b = 50;
+    ft_ultimate_div_mod(&a, &b);
+    printf("value i= %d\n", a);
+    printf("value i= %d\n", b);
+    return (0);
+}*/

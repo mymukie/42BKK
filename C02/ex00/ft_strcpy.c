f3e1f6465b14ded0f44345d10cf8186b2649a080
mymukie <tay_tachapon@hotmail.co.th>
2023-07-26 13:49:23 +0700

    ft_strcpy.c

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: tmulmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 11:57:31 by tmulmane          #+#    #+#             */
-/*   Updated: 2023/07/25 12:09:04 by tmulmane         ###   ########.fr       */
+/*   Updated: 2023/07/26 13:49:15 by tmulmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,32 @@ char *ft_strcpy(char *dest, char *src)
         dest[i] = src[i];
         i++;
     }
+    return (dest);
+}
+/*
+#include <unistd.h>
+char ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(void)
+{
+    int len;
+    char A[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    char B[20];
     
+    len = 0;
+
+    ft_strcpy(B, A);
+    while(B[len])
+    {
+        ft_putchar(B[len]);
+        len++;
+    }
+    write(1, "\n", 1);
+
     return (0);
 }
+*/
+

0c7aea9b7b40b997c5f34ed4c503232a724f498e
mymukie <tay_tachapon@hotmail.co.th>
2023-07-26 14:23:34 +0700

    ft_strncpy.c

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmulmane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 13:50:10 by tmulmane          #+#    #+#             */
+/*   Updated: 2023/07/26 14:23:24 by tmulmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    int i;
+
+    i = 0;
+    while(i <= n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    return (dest);
+}
+/*
+#include <unistd.h>
+
+char ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(void)
+{
+    int len;
+    unsigned int n;
+    char A[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\0'};
+    char B[20];
+    
+    n = 3;
+    len = 0;
+    ft_strncpy(B, A, n);
+    while(B[len])
+    {
+        ft_putchar(B[len]);
+        len++;
+    }
+    
+    return (0);
+}*/

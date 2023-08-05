b875627145de4796e2422ba90b2d68f4796ef583
mymukie <tay_tachapon@hotmail.co.th>
2023-08-06 01:11:12 +0700

    ft_str_is_alpha.c

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -3,55 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmulmane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tachapon <tachapon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 14:24:10 by tmulmane          #+#    #+#             */
-/*   Updated: 2023/07/26 23:36:01 by tmulmane         ###   ########.fr       */
+/*   Updated: 2023/08/06 01:09:18 by tachapon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <unistd.h>
 
 int ft_str_is_alpha(char *str)
 {
-    /*alphabetical characters = a-z 
-    if string has a-z will return 1
-    else return 0
-    emtry string return 1
-    create while loop check each character in wwhile loop string a-z
-    */
-    
     int i;
-    int j;
-    char p;
-
-	i = 0;
-	j = 0;
-    while(str[i])
+    
+    i = 0;
+    if(!str[i])
     {
-        while(str[j])
-        {
-            if(str[i] != str[j])
-            {
-                
-            }
-            else
-            {
-                p = '1';
-                return(p);
-            }
-            j++;
-        }
-        i++;
+        return (1);
     }
+    while (str[i])
+    {
+        if (((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+        {
+            i++;
+        }
+        else
+        {
+            return (0);
+        }
+    }
+    return (1);
 }
-
-#include <unistd.h>
-
 int main(void)
 {
-    char A[] = {'a','b','c','d', '\0'};
-    char B[] = {'a', 'b', '$','\0'};
+    char A[] = {"Hellworld"};
+    //char A[] = {'a','b','c','d', '\0'};
+    //char B[] = {'a', 'b', '$','\0'};
     ft_str_is_alpha(A);
     return (0);
 }
